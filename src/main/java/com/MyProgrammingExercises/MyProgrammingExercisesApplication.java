e64f4738c7ddab8a89c356ccedc5a6e1536b0589
Rohini <rohini_subramanian@outlook.com>
2021-09-14 23:03:13 +0800

    Create and initialise project

---
+++ src/main/java/com/MyProgrammingExercises/MyProgrammingExercisesApplication.java
@@ -0,0 +1,13 @@
+package com.MyProgrammingExercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProgrammingExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProgrammingExercisesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MyProgrammingExercises/MyProgrammingExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyProgrammingExercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProgrammingExercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
