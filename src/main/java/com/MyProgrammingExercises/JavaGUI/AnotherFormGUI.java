becfb5739be49c252f33008313963329fe870e59
Rohini <rohini_subramanian@outlook.com>
2022-03-23 22:29:55 +0800

    Create the GUI form using intellij SwingUIDesigner

---
+++ src/main/java/com/MyProgrammingExercises/JavaGUI/AnotherFormGUI.java
@@ -0,0 +1,30 @@
+package com.MyProgrammingExercises.JavaGUI;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AnotherFormGUI extends JFrame {
+    private JTextField usernameText;
+    private JPasswordField passwordField1;
+    private JButton submitButton;
+    private JLabel displayMsg;
+    private JPanel myJPanel;
+
+    public static void main(String[] args) {
+
+        AnotherFormGUI thisForm = new AnotherFormGUI();
+        thisForm.setContentPane(thisForm.myJPanel);
+        thisForm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        thisForm.setVisible(true);
+
+        thisForm.submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                thisForm.displayMsg.setText(thisForm.usernameText.getText() + " has entered password "
+                        +thisForm.passwordField1.getText());
+            }
+        });
+    } // END MAIN METHOD
+
+}
