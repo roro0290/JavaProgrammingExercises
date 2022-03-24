51ab9d1c3c2ee40ac11cea55f54451f6848c4d48
Rohini <rohini_subramanian@outlook.com>
2022-03-24 18:29:10 +0800

    Create update2DArray method which will be updated after each submit click

--- src/main/java/com/MyProgrammingExercises/ticTacToe/MainRunner.java
+++ src/main/java/com/MyProgrammingExercises/ticTacToe/MainRunner.java
@@ -3,9 +3,13 @@ package com.MyProgrammingExercises.ticTacToe;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /*
 Flow:
-1.
+1. Display page using displayMethod(myBoard)
+2. Click submit --> call update 2DArray method
  */
 public class MainRunner {
 
@@ -16,5 +20,14 @@ public class MainRunner {
 
         TicTacToeBoard myBoard = new TicTacToeBoard();
         utilMethods.displayMethod(myBoard);
+
+        String[][] board2DArray = new String[3][3];
+        myBoard.getSubmit().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                utilMethods.update2DArray(myBoard,board2DArray);
+                myBoard.getDisplayMsg().setText("clicked");
+            }
+        });
     }
 }
--- src/main/java/com/MyProgrammingExercises/ticTacToe/UtilMethods.java
+++ src/main/java/com/MyProgrammingExercises/ticTacToe/UtilMethods.java
@@ -13,4 +13,27 @@ public class UtilMethods {
         myBoard.setVisible(true);
     }
 
+    /*
+    Input board and boardArray
+    Fetch details from board
+    Update the boardArray
+    Check boardArray to see if it meets the conditions
+     */
+    public static void update2DArray(TicTacToeBoard myBoard,String[][] boardArray){
+        // ROW 1
+        boardArray[0][0] = myBoard.getTextField1().getText();
+        boardArray[0][1] = myBoard.getTextField2().getText();
+        boardArray[0][2] = myBoard.getTextField3().getText();
+
+        // ROW 2
+        boardArray[1][0] = myBoard.getTextField4().getText();
+        boardArray[1][1] = myBoard.getTextField5().getText();
+        boardArray[1][2] = myBoard.getTextField6().getText();
+
+        // ROW 3
+        boardArray[2][0] = myBoard.getTextField7().getText();
+        boardArray[2][1] = myBoard.getTextField8().getText();
+        boardArray[2][2] = myBoard.getTextField9().getText();
+    }
+
 }
